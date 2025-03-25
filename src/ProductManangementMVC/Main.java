d00a570b6b6acbffa6603f0c205b667b6cbf189d
Seangly <seanglychou62@gmail.com>
2025-03-25 16:46:57 +0700

    first add

--- src/ProductManangementMVC/Main.java
+++ src/ProductManangementMVC/Main.java
@@ -4,11 +4,17 @@ import ProductManangementMVC.Controll.ProductController;
 import ProductManangementMVC.Model.ProjectDAOImpl;
 import ProductManangementMVC.View.ProductView;
 
+
 public class Main {
     public static void main(String[] args) {
         ProductController productController = new ProductController(new ProjectDAOImpl(), new ProductView());
-            productController.showProducts();
+            try{
+                productController.showProducts();
 
+            }catch(IndexOutOfBoundsException e)
+            {
+                e.printStackTrace();
+            }
 
     }
 }
--- src/ProductManangementMVC/View/ProductView.java
+++ src/ProductManangementMVC/View/ProductView.java
@@ -1,17 +1,32 @@
 package ProductManangementMVC.View;
 
 import ProductManangementMVC.Model.Product;
+import org.nocrala.tools.texttablefmt.BorderStyle;
+import org.nocrala.tools.texttablefmt.Table;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public class ProductView {
     public static void displayProducts(List<Product> products)
     {
         System.out.println("Product List");
+        Table t = new Table(5, BorderStyle.UNICODE_BOX_DOUBLE_BORDER);
+        t.setColumnWidth(1,10,15);
+        t.setColumnWidth(2,10,15);
+        t.setColumnWidth(3,10,15);
+        t.setColumnWidth(4,10,15);
+        t.setColumnWidth(5,10,15);
         for(Product product : products)
         {
-            System.out.println(product);
+           t.addCell(String.valueOf(product.getId()));
+           t.addCell(product.getName());
+           t.addCell(String.valueOf(product.getUnitPrice()));
+           t.addCell(String.valueOf(product.getQty()));
+           t.addCell(String.valueOf(product.getData()));
         }
+        System.out.println(t.render());
 
     }
     public void showMessage(String message)
