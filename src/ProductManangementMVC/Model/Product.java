7533209b94c51ab3710fe547f6258abac6956337
UyChakriya <uychakriya002@gmail.com>
2025-03-26 08:12:00 +0700

    just test

--- src/ProductManangementMVC/Model/Product.java
+++ src/ProductManangementMVC/Model/Product.java
@@ -1,68 +1,57 @@
-package ProductManangementMVC.Model;
+import ProductManangementMVC.Model.Product;
+import ProductManangementMVC.Model.ProjectDAOImpl;
+import ProductManangementMVC.Model.Validate;
+import ProductManangementMVC.View.ProductView;
 import java.time.LocalDate;
-public class Product {
-    private int id;
-    private String name;
-    private double UnitPrice;
-    private int Qty;
-    private LocalDate data;
-    public Product(int id, String name, double UnitPrice, int Qty, LocalDate data) {
-        this.id = id;
-        this.name = name;
-        this.UnitPrice = UnitPrice;
-        this.Qty = Qty;
-        this.data = data;
-    }
+import java.util.List;
+
+public class ProductController {
+    private ProjectDAOImpl pim;
+    private ProductView pv;
+    Validate v = new Validate();
+    Constant c = new Constant();
 
 
-    public int getId() {
-        return id;
+    //start an application
+    public void startApplication() {
+        try {
+            List<Product> products = pim.getAllProduct();
+
+            pv.displayAllProductsAndMenu(products, this);
+        } catch (Exception e) {
+            System.err.println(c.RED + "An error occurred: " + e.getMessage() + c.RESET);
+        }
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int generateNewProductId(List<Product> products, List<Product> tempoLists) {
+        int maxDbId = products.stream()
+                .mapToInt(Product::getId)
+                .max()
+                .orElse(0);
+
+        int maxTempId = tempoLists.stream()
+                .mapToInt(Product::getId)
+                .max()
+                .orElse(0);
+        return Math.max(maxDbId, maxTempId) + 1;
+
     }
 
-    public String getName() {
-        return name;
+    //Create new Product
+    public void createNewProduct() {
+        try {
+            List<Product> initialProductLists = pim.getAllProduct();
+            List<Product> tempInsertProducts = List.of();
+            int newId = generateNewProductId(initialProductLists, tempInsertProducts);
+            Product product = pv.getInputForNewProduct(newId);
+            product.setId(newId);
+            product.setImportDate(LocalDate.now());
+            tempInsertProducts.add(product);
+            System.out.println(c.GREEN + "Product added to the temporary list. Use 'Save' to push to the database." + c.RESET);
+
+        } catch (Exception e) {
+            System.err.println(c.RED + "An error occurred while creating the product: " + e.getMessage() + c.RESET);
+        }
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getUnitPrice() {
-        return UnitPrice;
-    }
-
-    public void setUnitPrice(double unitPrice) {
-        UnitPrice = unitPrice;
-    }
-
-    public int getQty() {
-        return Qty;
-    }
-
-    public void setQty(int qty) {
-        Qty = qty;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", UnitPrice=" + UnitPrice +
-                ", Qty=" + Qty +
-                ", data=" + data +
-                '}';
-    }
 }
