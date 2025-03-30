e209af9466d2e40c7a0cc7804288a4c4de209da3
NHA-Here <san387561@gmail.com>
2025-03-30 22:48:56 +0700

    Add files via upload
    Last Commit

---
+++ src/ProductManagementMVC/Main.java
@@ -0,0 +1,8 @@
+import ProductManagementMVC.Controll.ProductController;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductController productController = new ProductController();
+        productController.start();
+    }
+}
